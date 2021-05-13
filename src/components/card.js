9f393d0b281f4cae982272def0f7dc0f2b947498
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-05-13 00:11:44 -0500

    Card component basics complete, and API call added with initial values

--- src/components/card.js
+++ src/components/card.js
@@ -1,15 +1,28 @@
 import React, { useState, useEffect } from 'react'
+// import { BASE_URL, API_KEY } from './constants'
+import axios from 'axios'
 
 export default function Card(props){
+    const initialState = 
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+        .then(function (response) {
+          setCardInfo(response.data);
+        })
+        .catch(function (error) {
+          // handle error
+            console.log(error);
+        })
+    const [cardInfo, setCardInfo] = useState(initialState);
+
     return (
         <div className='cardContainer'>
             <div className='imgContainer'>
-                <img src={astronomyImage} alt='Astronomy'></img>
+                <img src={cardInfo.hdurl} alt='Astronomy'></img>
             </div>
             <div className="textContent">
-                <h1>{title}</h1>
-                <p>{date}</p>
-                <p>{articleText}</p>
+                <h1>{cardInfo.title}</h1>
+                <p>{cardInfo.date}</p>
+                <p>{cardInfo.explanation}</p>
             </div>
             <button>Change Article</button> {/* need to add onclick and change of article effect*/}
         </div>
