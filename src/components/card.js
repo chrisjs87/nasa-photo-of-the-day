61fc57f8a0dedf1003c90504d5b80673fea410d5
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-05-13 09:24:15 -0500

    Test Commit

--- src/components/card.js
+++ src/components/card.js
@@ -3,16 +3,51 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 export default function Card(props){
-    const initialState = 
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
-        .then(function (response) {
-          setCardInfo(response.data);
-        })
-        .catch(function (error) {
-          // handle error
-            console.log(error);
-        })
-    const [cardInfo, setCardInfo] = useState(initialState);
+    // const initialState = 
+    //     axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+    //         .then(function (response) {
+    //         setCardInfo(response.data);
+    //         })
+    //         .catch(function (error) {
+    //         // handle error
+    //             console.log(error);
+    //         })
+    const [cardInfo, setCardInfo] = useState(null);
+    useEffect(() => {
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+                .then(function (response) {
+                setCardInfo(response.data);
+                })
+                .catch(function (error) {
+                // handle error
+                    console.log(error);
+                })
+            }, [cardInfo])
+    console.log(cardInfo);
+
+   
+
+    
+
+
+    // function switchArticle(){
+    //     useEffect(() => {
+    //     axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+    //     .then(function (response) {
+    //       // handle success
+    //     //   console.log(response.data);
+    //       setCardInfo(response.data);
+    //     })
+    //     .catch(function (error) {
+    //       // handle error
+    //     //   console.log(error);
+    //     })
+    //     return () => {
+    //       //Component will unmount
+    //       console.log('component unmounting, the id was ' + friendId)
+    //     }
+    //   }, [])
+    // }
 
     return (
         <div className='cardContainer'>
