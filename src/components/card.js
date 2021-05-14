940c07b429bd8ae959fce9f1d4ce8bc9b62af6e3
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-05-13 21:46:42 -0500

    Added some styling and changed some aspects of my compontents to involve styled components

--- src/components/card.js
+++ src/components/card.js
@@ -1,20 +1,32 @@
 import React from 'react'
+import styled from 'styled-components'
 // import { BASE_URL, API_KEY } from './constants'
 // import axios from 'axios'
+
+const StyledCard = styled.div`
+    color: ${props => props.theme.black};
+    width: 80%;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    justify-content:center;
+`;
+
+const StyledImg = styled.img`
+    width: 50%;
+`;
 
 export default function Card(props){
     const { info } = props;
     return (
-        <div className='cardContainer'>
-            <div className='imgContainer'>
-                <img src={info.hdurl} alt='Astronomy'></img>
-            </div>
+        <StyledCard>
+            <StyledImg src={info.hdurl} alt='Astronomy'></StyledImg>
             <div className="textContent">
                 <h1>{info.title}</h1>
                 <p>{info.date}</p>
                 <p>{info.explanation}</p>
             </div>
             <button>Change Article</button> {/* need to add onclick and change of article effect*/}
-        </div>
+        </StyledCard>
     );
 }
